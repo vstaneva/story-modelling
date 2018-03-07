b85fdde9c8c6adfc9d4a00cf5f7692745aadd56b
Valeria Staneva <vstaneva@mit.edu>
2018-03-07 14:01:24 -0500

    Added arrow + testing

--- distibutions.py
+++ distibutions.py
@@ -18,14 +18,12 @@ Class Process:
             probs = [1/size(options)]*size(options)
         else: #normalise the probabilities
             probs = [float(prob)/sum(probs) for prob in probs]
-        dist_list = []
-        for i in xrange(size(options)):
-            prob, option = probs[i], options[i]
-            dist_list.append((prob,option))
-        return dist_list
+        dist_dict = {}
+        for i  in xrange(size(options)):
+            dist_dict[options[i]] = probs[i]
+        return dist_dict
         
-    
-    def __init__(self, name, dist_options, dist_probs=None, data, parent):
+    def __init__(self, name, parent, dist_options, dist_probs=None, data=None):
         self.name = name
         self.distribution = options_dist(dist_options, dist_probs)
         self.data = data
@@ -33,12 +31,17 @@ Class Process:
         self.arrows_out = []
         self.parent = parent
         
-    def add_arrow(self, prob, next_data):
+    def add_arrow(self, option, next_data):
         next_name = next_data[0]
         next_dist_options = next_data[1]
         next_dist_prob = next_data[2]
         next_datasheet = next_data[3]
         next_parent = self
         next = Process(next_name, next_dist_options, next_dist_prob, next_datasheet, next_data, self)
+        prob = self.dist[option]
         self.arrows_out.append((prob, next))
 
+# Some testing
+student = Process("student", None, ["cheat", "not cheat"])
+student.add_arrow("cheat",     ["first coin", ["heads", "tails"], None, None])
+student.add_arrow("not cheat", ["first coin", ["heads", "tails"], None, None])  
